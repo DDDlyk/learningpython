772b9abbd9593d3cd2c3630a2d58c8eb4d391157
DDD <529652526@qq.com>
2019-02-16 09:34:32 +0800

    2019年2月16日09:34:20

--- 08_面向对象/ddd_14_士兵突击_01_枪类.py
+++ 08_面向对象/ddd_14_士兵突击_01_枪类.py
@@ -8,7 +8,26 @@ class Gun:
         # 2.子弹的数量
         self.bullet_count = 0
 
-    def add_bullt(self, count):
+    def add_bullet(self, count):
 
         self.bullet_count += count
 
+    def shoot(self):
+        #  1.判断子弹数量
+        if self.bullet_count <= 0:
+            print ("[%s]没有子弹了..." % self.model)
+
+            return
+
+        # 2.发射子弹，-1
+        self.bullet_count -= 1
+
+        # 3.提示发射信息
+        print ("[%s] 突突突... [%d]" % (self.model, self.bullet_count))
+
+
+# 1.创建抢对象
+ak47 = Gun("ak47")
+
+ak47.add_bullet(50)
+ak47.shoot()
