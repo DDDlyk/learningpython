12a390e3e41615e821b333c29b90664478df88b3
DDD <529652526@qq.com>
2019-02-26 10:45:06 +0800

    2019年2月26日10:45:01

---
+++ 飞机大战/ddd_10_监听退出事件.py
@@ -0,0 +1,60 @@
+import pygame
+
+# 游戏的初始化
+pygame.init()
+
+# 创建游戏的窗口 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+# 绘制背景图像
+bg = pygame.image.load("./images/background.png")
+screen.blit(bg, (0, 0))
+# pygame.display.update()
+
+# 绘制英雄的飞机
+hero = pygame.image.load("./images/hero1.png")
+screen.blit(hero, (150, 300))
+
+# 可以在所有绘制工作完成之后，统一调用update方法
+pygame.display.update()
+
+# 创建一个始终对象
+clock = pygame.time.Clock()
+
+# 1.定义rect记录飞机的初始位置
+hero_rect = pygame.Rect(150, 300, 100, 124)
+# 游戏循环 -> 意味着游戏的正式开始！
+while True:
+
+    clock.tick(60)
+
+    # 监听事件
+    for event in pygame.event.get():
+
+        # 判断事件类型是否是退出事件
+        if event.type == pygame.QUIT:
+            print("游戏退出...")
+
+            # quit 卸载所有的模块
+            pygame.quit()
+
+            # exit()直接终止正在执行的程序
+            exit()
+
+
+    # 2.修改飞机的位置
+    hero_rect.y -= 1
+
+    # 判断飞机位置
+    if hero_rect.y <= -100:
+        hero_rect.y = 700
+
+    # 3.调用blit方法绘制图像
+    screen.blit(bg, (0, 0))
+    screen.blit(hero, hero_rect)
+
+    # 4.调用update方法更新显示
+    pygame.display.update()
+
+pygame.quit()
+
