6f2b998c40bf9c2c36314bdc54aa38c9c41d44f2
DDD <529652526@qq.com>
2019-03-03 10:58:51 +0800

    2019年3月3日10:58:46

---
+++ mutiply/10-共享全局变量的问题-资源竞争.py
@@ -0,0 +1,53 @@
+import threading
+import time
+
+# 定义一个全局变量
+g_num = 0
+
+
+def test1(num):
+    global g_num
+    # 上锁,如果之前没有被上锁，那么此时，上锁成功
+    # 如果上锁之前，已经被上锁了，那么此时会堵塞在这里，直到这个锁被解开位置
+    mutex.acquire()
+    for i in range(num):
+        g_num += 1
+    # 解锁
+    mutex.release()
+    print("------in test1 g_num=%d-----" % g_num)
+
+
+def test2(num):
+    global g_num
+    # 上锁,如果之前没有被上锁，那么此时，上锁成功
+    # 如果上锁之前，已经被上锁了，那么此时会堵塞在这里，直到这个锁被解开位置
+    mutex.acquire()
+    for i in range(num):
+        g_num += 1
+    # 解锁
+    mutex.release()
+    print("------in test2 g_unm=%d-----" % g_num)
+
+
+# 创建一个互斥锁，默认是没有上锁的
+mutex = threading.Lock()
+
+
+def main():
+    t1 = threading.Thread(target=test1, args=(10000000,))
+    t2 = threading.Thread(target=test2, args=(10000000,))
+
+    t1.start()
+    # time.sleep(1)
+
+    t2.start()
+    # time.sleep(1)
+
+    # 等待上面的线程执行完毕
+    time.sleep(2)
+    print("-----in main Thread g_num = %d ----" % g_num)
+
+
+if __name__ == '__main__':
+    main()
+
